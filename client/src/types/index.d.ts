4c8ab167e166767ae0e1ae6ab81fe2dbc57f438c
mstout60 <mstout@eprospector.com>
2025-08-21 11:04:22 -0500

    Implement Purchase and Expense Summry charts using Recharts

--- client/src/types/index.d.ts
+++ client/src/types/index.d.ts
@@ -6,11 +6,23 @@ import {
   SalesSummary,
 } from "@prisma/client";
 
+// Interface
+interface ExpenseByCategorySummary {
+  expenseByCategorySummaryId: string;
+  category: string;
+  amount: string;
+  date: string;
+}
+
 // Types
 declare type DashboardMetrics = {
   popularProducts: Products[];
   salesSummary: SalesSummary[];
   purchaseSummary: PurchaseSummary[];
   expenseSummary: ExpenseSummary[];
-  expenseByCategorySummary: ExpenseByCategory[];
+  expenseByCategorySummary: ExpenseByCategorySummary[];
+};
+
+declare type ExpenseSums = {
+  [category: string]: number;
 };
