25cd56a811cd9dc1ea62254ea0fcd468f6495a62
mstout60 <mstout@eprospector.com>
2025-08-21 18:07:57 -0500

    Implement Expense Controller and Router

--- server/dist/src/index.js
+++ server/dist/src/index.js
@@ -13,6 +13,7 @@ const morgan_1 = __importDefault(require("morgan"));
 const dashboardRoutes_1 = __importDefault(require("./routes/dashboardRoutes"));
 const productRoutes_1 = __importDefault(require("./routes/productRoutes"));
 const userRoutes_1 = __importDefault(require("./routes/userRoutes"));
+const expnseRoutes_1 = __importDefault(require("./routes/expnseRoutes"));
 /* Configurations */
 dotenv_1.default.config();
 const app = (0, express_1.default)();
@@ -27,6 +28,7 @@ app.use((0, cors_1.default)());
 app.use("/dashboard", dashboardRoutes_1.default);
 app.use("/products", productRoutes_1.default);
 app.use("/users", userRoutes_1.default);
+app.use("/expenses", expnseRoutes_1.default);
 /* Server */
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import morgan from "morgan";
 import dashboardRoutes from "./routes/dashboardRoutes";
 import productRoutes from "./routes/productRoutes";
 import userRoutes from "./routes/userRoutes";
+import expenseRoutes from "./routes/expnseRoutes"
 
 /* Configurations */
 dotenv.config();
@@ -25,6 +26,7 @@ app.use(cors());
 app.use("/dashboard", dashboardRoutes);
 app.use("/products", productRoutes);
 app.use("/users", userRoutes);
+app.use("/expenses", expenseRoutes)
 
 /* Server */
 const port = process.env.PORT || 3001;
